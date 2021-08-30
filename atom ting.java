174463ebcf97672bc3444a83471041001e3bbc86
Magnus2002 <73287545+Magnus2002@users.noreply.github.com>
2021-08-30 19:32:57 +0200

    Add files via upload

---
+++ atom ting.java
@@ -0,0 +1,17 @@
+class MitProgram{
+
+  public static void main(String[] args){
+    int[] talliste = {1,2,6,11,16,23,27,35,40,43,44,49,58};
+
+    int indexAf6 = findPlads(6,talliste);
+    System.out.println(indexAf6);
+
+  }
+
+static int findPlads(int tal, int[] talliste){
+  int index = 0;
+
+  return index;
+}
+
+}
